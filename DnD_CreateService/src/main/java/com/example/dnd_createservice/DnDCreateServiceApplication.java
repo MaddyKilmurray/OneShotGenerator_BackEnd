00cd7165ce5583dccea726303403bef4ef89c352
Maddy <maddy.kilsbymcmurray@gmail.com>
2021-11-25 18:47:32 +0000

    Create Service added

---
+++ DnD_CreateService/src/main/java/com/example/dnd_createservice/DnDCreateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.dnd_createservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnDCreateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DnDCreateServiceApplication.class, args);
+    }
+
+}
---
+++ DnD_CreateService/src/test/java/com/example/dnd_createservice/DnDCreateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dnd_createservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnDCreateServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
