fcfa0ba40fe283acceca6ed6c7069af4ee0e3cf1
Maddy <maddy.kilsbymcmurray@gmail.com>
2021-11-26 17:18:59 +0000

    Security added to gateway

--- DnD_Gateway/src/main/java/com/kilmurray/dnd_gateway/GatewayConfiguration.java
+++ DnD_Gateway/src/main/java/com/kilmurray/dnd_gateway/GatewayConfiguration.java
@@ -1,4 +1,24 @@
 package com.kilmurray.dnd_gateway;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class GatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/api/character/**")
+                        .uri("lb://CHARACTER-SERVICE"))
+                .route(p -> p.path("/api/create/**")
+                        .uri("lb://CREATE-SERVICE"))
+                .route(p -> p.path("/api/random/**")
+                        .uri("lb://RANDOMISER-SERVICE"))
+                .route(p -> p.path("/api/user/**")
+                        .uri("lb://USER-SERVICE"))
+                .build();
+    }
 }
