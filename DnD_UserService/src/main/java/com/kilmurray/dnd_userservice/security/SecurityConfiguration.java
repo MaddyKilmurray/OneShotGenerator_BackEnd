59fb366e706e5852aaf207840c036ed02162e344
Maddy <maddy.kilsbymcmurray@gmail.com>
2021-12-05 20:47:47 +0000

    User Service okta infrastructure added

--- DnD_UserService/src/main/java/com/kilmurray/dnd_userservice/security/SecurityConfiguration.java
+++ DnD_UserService/src/main/java/com/kilmurray/dnd_userservice/security/SecurityConfiguration.java
@@ -1,51 +1,29 @@
 package com.kilmurray.dnd_userservice.security;
 
-import com.kilmurray.dnd_userservice.controller.UserService;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private final UserService userService;
-    private final BCryptPasswordEncoder encoder;
-    private final Environment environment;
-
-    public SecurityConfiguration(UserService userService, BCryptPasswordEncoder encoder, Environment environment) {
-        this.userService = userService;
-        this.encoder = encoder;
-        this.environment=environment;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        // @formatter:off
         http
-                .authorizeRequests()
-                .antMatchers("/users")
-                .permitAll()
-                .antMatchers("/user_details")
-                .permitAll()
-                .antMatchers("/users/**")
-                .authenticated()
+                .authorizeExchange()
+                .anyExchange().authenticated()
                 .and()
-                .addFilter(getAuthenticationFilter());
-    }
+                .oauth2ResourceServer()
+                .jwt();
 
-    private AuthenticationFilter getAuthenticationFilter() throws Exception {
-        AuthenticationFilter authenticationFilter = new AuthenticationFilter(userService,environment,authenticationManager());
-        authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
-        return authenticationFilter;
-    }
+        Okta.configureResourceServer401ResponseBody(http);
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(encoder);
+        return http.build();
+        // @formatter:on
     }
 }
