ec9030f7c5e3fc0f4c0364115bb2b3d88a95bc79
Maddy <maddy.kilsbymcmurray@gmail.com>
2021-12-05 20:44:04 +0000

    Gateway security added

--- DnD_Gateway/src/main/java/com/kilmurray/dnd_gateway/SecurityConfiguration.java
+++ DnD_Gateway/src/main/java/com/kilmurray/dnd_gateway/SecurityConfiguration.java
@@ -1,38 +1,46 @@
 package com.kilmurray.dnd_gateway;
 
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-@Configuration
-public class GatewayConfiguration {
+import java.util.Arrays;
+import java.util.List;
 
-    private final Environment environment;
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfiguration {
 
-    public GatewayConfiguration(Environment environment) {
-        this.environment = environment;
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        // @formatter:off
+        http
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2Login()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+        // @formatter:on
     }
 
     @Bean
-    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(p -> p.path("/api/character/**")
-                        .filters(f -> f.filter(new AuthorisationHeadersFilter(environment).apply(new AuthorisationHeadersFilter.Config())))
-                        .uri("lb://CHARACTER-SERVICE"))
-                .route(p -> p.path("/api/create/view")
-                        .uri("lb://CREATE-SERVICE"))
-                .route(p -> p.path("/api/create/save")
-                        .filters(f -> f.filter(new AuthorisationHeadersFilter(environment).apply(new AuthorisationHeadersFilter.Config())))
-                        .uri("lb://CREATE-SERVICE"))
-                .route(p -> p.path("/api/random/**")
-                        .uri("lb://RANDOMISER-SERVICE"))
-                .route(p -> p.path("/api/user/register")
-                        .uri("lb://USER-SERVICE"))
-                .route(p -> p.path("/api/user/**")
-                        .filters(f -> f.filter(new AuthorisationHeadersFilter(environment).apply(new AuthorisationHeadersFilter.Config())))
-                        .uri("lb://USER-SERVICE"))
-                .build();
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("*"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
     }
 }
